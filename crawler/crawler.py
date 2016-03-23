78e3e4d266ba9524820d8bc463f56cf176a57983
Anastasia Iovleva <ionastasi@gmail.com>
2016-03-23 10:27:14 +0300

    first iteration of crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -9,24 +9,24 @@ import sys
 import log
 import download
 import clean
+from config import *
 
-
-HTMLS_INDEX = '../data/html/.index'
+'''
+Вводить имя файла в clean list без ключа --file, а сразу
+'''
 
 
 def parse_argument(argv):
     parser = argparse.ArgumentParser(
         prog='crawler',
-        description='''Download and clean posts from geektimes.ru.
-                       Because of reforwarding, a few posts from
-                       habrababr.ru and megamozg.ru too.'''
+        description='''Download and clean posts from geektimes.ru.'''
     )
     parser.add_argument(
         '--log',
         default='error',
         choices=['critical', 'error',  'debug'],
         dest='log_level',
-        help="changing log level, 'error' for default"
+        help="log level, 'error' is default value"
     )
 
     subparsers = parser.add_subparsers(
@@ -42,43 +42,43 @@ def parse_argument(argv):
         '--print-ids',
         action='store_true',
         dest='print_ids',
-        help='set this if you want ids printed out'
+        help="print articles' ids"
     )
     update_subs = update.add_subparsers(
         dest='update_mode'
     )
     update_last = update_subs.add_parser(
         'last',
-        description="Download last 1000 posts, if they wasn't downloaded earlier.",
+        description="Download latest 1000 articles, if they have not been downloaded yet.",
         help='download last 1000 posts'
     )
     update_range = update_subs.add_parser(
         'range',
-        description='''Download posts in some range.
+        description='''Download range of posts.
                        Stops if starts downloading already downloaded posts.
                        Of course, you should set the range by --from and --to.''',
-        help='download posts in some range'
+        help='download range of posts'
     )
     update_range.add_argument(
         '--from',
         required=True,
         type=int,
         dest='start_down',
-        help='where range of downloading starts'
+        help='start of range'
     )
     update_range.add_argument(
         '--to',
         required=True,
         type=int,
         dest='end_down',
-        help='where range of downloading ends'
+        help='end of range'
     )
 
 
     clean = subparsers.add_parser(
         'clean',
         help='cleaning pages',
-        description='''Clean pages from tags and etc.
+        description='''Extract text of articles and meta information.
                        You can clean all pages, or some range of them, or some list.'''
     )
     clean_subs = clean.add_subparsers(
@@ -87,12 +87,12 @@ def parse_argument(argv):
     clean_all = clean_subs.add_parser(
         'all',
         help='parse all htmls',
-        description="Clean all htmls from tags and etc, if they weren't cleaned earlier."
+        description="Process articles, it they have been cleaned before."
     )
     clean_range = clean_subs.add_parser(
         'range',
-        help='parse htmls in some range',
-        description='''Clean htmls in some range, skipping already cleaned htmls.
+        help='clean range of htmls',
+        description='''Clean range of htmls., skipping already cleaned htmls.
                        Of course you should set the range by --from and --to.'''
     )
     clean_range.add_argument(
@@ -111,28 +111,26 @@ def parse_argument(argv):
     )
     clean_list = clean_subs.add_parser(
         'list',
-        help='parse htmls from some list',
+        help='clean listed articles with listed ids',
         description='''Clean htmls from some list you give with --file key.'''
     )
     clean_list.add_argument(
-        '--file',
-        required=True,
-        type=str,
         dest='clean_file',
-        help='cleaning posts from list in this file'
-    )
+        metavar='file',
+        type=str,
+        help='cleaning posts from list in this file')
 
     update.add_argument(
         '--force',
         action='store_true',
         dest='update_force',
-        help='downloading pages even if they were downloaded earlier'
+        help='download pages even if they were downloaded earlier'
     )
     clean.add_argument(
         '--force',
         action='store_true',
         dest='clean_force',
-        help='cleaning pages even if they were cleaned earlier'
+        help='clean pages even if they were cleaned earlier'
     )
 
     if 'update' in sys.argv:
@@ -143,15 +141,17 @@ def parse_argument(argv):
         if 'range' not in sys.argv and 'all' not in sys.argv and 'list' not in sys.argv:
             clean.print_help()
             sys.exit(1)
-    else:
-        parser.print_help()
-        sys.exit(1)
+    #else:
+    #    parser.print_help()
+    #    sys.exit(1)
 
     return parser.parse_args()
 
 
 def main():
     args = parse_argument(sys.argv)
+    if not args:
+        return
 
     log.config(log.level(args.log_level))
 
@@ -159,19 +159,18 @@ def main():
         if args.update_mode == 'last':
             download.download_last(args.print_ids, args.update_force)
         if args.update_mode == 'range':
-            download.download_range(min(args.start_down, args.end_down),
-                                    max(args.start_down, args.end_down),
+            download.download_range(args.start_down, args.end_down,
                                     args.print_ids, args.update_force)
 
     if args.parser_mode == 'clean':
         if args.clean_mode == 'list':
             ids = list(map(int, open(args.clean_file).readlines()))
-        if args.clean_mode == 'all':
+        elif args.clean_mode == 'all':
             htmls = list(map(int, open(HTMLS_INDEX).readlines()))
             if not htmls:
                 exit()
             ids = [x for x in range(min(htmls), max(htmls) + 1)]
-        if args.clean_mode == 'range':
+        elif args.clean_mode == 'range':
             first = min(args.start_clean, args.end_clean)
             last = max(args.start_clean, args.end_clean)
             ids = [x for x in range(first, last + 1)]
