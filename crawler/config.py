a1751fdd2dc6e18a9b2aa255a5df66b6e82ab390
Anastasia Iovleva <ionastasi@gmail.com>
2016-03-26 13:00:08 +0300

    merging

---
+++ crawler/config.py
@@ -0,0 +1,7 @@
+# some constants that uses in several files
+KB_SIZE = 1024
+HTML_DIR = '../data/html/'
+TEXT_DIR = '../data/text/'
+TEXTS_INDEX = TEXT_DIR + '.index'
+HTMLS_INDEX = HTML_DIR + '.index'
+DATE_FORMAT = "%Y-%m-%d"
