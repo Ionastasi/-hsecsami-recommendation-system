fb70a8186c74d0ab4f786130c8c5d0ca068ce49d
Anastasia Iovleva <ionastasi@gmail.com>
2016-05-16 22:42:48 +0300

    fix grit search

--- source/classifier.py
+++ source/classifier.py
@@ -7,11 +7,13 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
-import sklearn
 import numpy as np
+from time import time
 
 from config import *
 from utils import *
+
+MARKS_FILE = '../data/marks.txt'
 
 class DataSet:
     # what about numpy?..
@@ -21,31 +23,9 @@ class DataSet:
         self.target_names = list()
 
 
-def later_i_will_use_it():
-    # grid search
-    parameters = {'tfidf__use_idf': (True, False),
-                  'clf__alpha': (1e-2, 1e-3),
-                  'cls__penalty': ('none', 'l2', 'l1', 'elasticnet')
-                 }
-    gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
-    gs_clf = gs_clf.fit(train.data, train.target)
-    train.target_names[gs_clf.predict(['God is love'])]
-    predicted = gs_clf.predict(test.data)
-    np.mean(predicted == test.target)
-    print('Best parameters:')
-    params = gs_clf.get_params()
-    print(params['estimator__vect'])
-    print(params['estimator__tfidf'])
-    print(params['estimator__clf'])
-    best_parameters, score, _ = max(gs_clf.grid_scores_, key=lambda x: x[1])
-    for param_name in sorted(parameters.keys()):
-        print("%s: %r" % (param_name, best_parameters[param_name]))
-    print(score)
-
-
 def get_a_structure(subset='train'):
     result = DataSet()
-    with open('../data/marks.txt') as file:
+    with open(MARKS_FILE) as file:
         lines = file.readlines()
     for_train = 4 * len(lines) // 5
     if subset == 'train':
@@ -64,26 +44,47 @@ def get_a_structure(subset='train'):
     return result
 
 
+def scorer(estimator, X, Y):
+    metric = metrics.roc_auc_score
+    return metric(Y, estimator.predict_proba(X)[:, 1])
+
+
 def main():
-    print(sklearn.__version__)
     print('Making a train set')
     train = get_a_structure(subset='train')
     print('Making a test set')
     test = get_a_structure(subset='test')
 
     text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-                         ('tfidf', TfidfTransformer(use_idf=False)),
-                         ('clf', SGDClassifier(penalty='l2',
-                                               alpha=1e-3, n_iter=5, random_state=42))
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', SGDClassifier(loss='log', n_iter=5, random_state=42))
                         ])
-    print('Start training')
-    text_clf = text_clf.fit(train.data, train.target)
-    print('Start predicting')
-    predicted = text_clf.predict(test.data)
-    print('Score =', np.mean(predicted == test.target))
+    parameters = {'clf__penalty': ['l1'], #['none', 'l2', 'l1', 'elasticnet'],
+                  'clf__l1_ratio': [0.0], #[0.0, 0.01, 0.05, 0.10, 0.2, 0.3, 0.4, 0.5],
+                  'clf__alpha': [0.001], #[0.001, 0.0001, 0.00001, 0.000001, 0.0000001],
+                  'tfidf__use_idf': [True] #(True, False)
+                 }
 
+    searcher = GridSearchCV(estimator=text_clf,
+                            param_grid=parameters,
+                            scoring=scorer,
+                            cv=5,
+                            n_jobs=-1
+                           )
+    print('Training... ', end='')
+    t = time()
+    searcher.fit(train.data, train.target)
+    print(round(time() - t, 3))
+
+    print("\nbest score:", searcher.best_score_)
+    print("best parameters:", searcher.best_params_, sep='\n')
+    best_cls = searcher.best_estimator_
+    print("\npredicted quality".upper())
+    predicted = best_cls.predict(test.data)
+    print("score:", scorer(best_cls, test.data, test.target))
+    print("mean:", np.mean(predicted == test.target))
     print(metrics.classification_report(test.target, predicted,
-                                        target_names=test.target_names))
+                                         target_names=test.target_names))
     print(metrics.confusion_matrix(test.target, predicted))
 
 
