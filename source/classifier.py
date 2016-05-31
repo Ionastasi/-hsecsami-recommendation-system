da8fefe9131256a0913b73580cc566663af263e1
Anastasia Iovleva <ionastasi@gmail.com>
2016-05-31 16:33:34 +0300

    almoust finish version + notebook

--- source/classifier.py
+++ source/classifier.py
@@ -2,32 +2,67 @@
 # -*- coding: utf-8 -*-
 # ionastasi@gmail.com
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import SGDClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import LeaveOneOut
 from sklearn import metrics
 import numpy as np
 from time import time
 from random import shuffle
 from collections import namedtuple
+import argparse
 
+import log
 from config import *
 from utils import *
 
-'''
-посмотрть статистику, которая будет больше внимания уделять ошибка 1/2 рода, когда neg считается за pos
-'''
-
 Articles = namedtuple('Articles', ['train', 'test'])
 Marked = namedtuple('Marked', ['positive', 'negative'])
+BestClassifier = namedtuple('BestClassifier', ['clf', 'name'])
+
+
+def parse_argument():
+    classifier = argparse.ArgumentParser(
+        prog='classifier',
+        description="Training the linear classifier for articles.",
+    )
+    classifier.add_argument(
+        '--log',
+        default='error',
+        choices=['critical', 'error',  'debug'],
+        dest='log_level',
+        help="log level, 'error' is default value"
+    )
+    classifier.add_argument(
+        '--steps',
+        type=int,
+        default=1,
+        dest='steps',
+        help="run learning of classifiers several times"
+    )
+
+    return classifier.parse_args()
+
 
 class DataSet:
     # what about numpy?..
     def __init__(self):
         self.data = list()
+        self.titles = list()
         self.target = list()
         self.target_names = list()
+
+
+class ClassifierData:
+    def __init__(self):
+        self.name = ''
+        self.clf = ''
+        self.parameters = ''
+
 
 def get_marked():
     pos, neg = list(), list()
@@ -42,11 +77,10 @@ def get_marked():
             else:
                 neg.append([post_id, mark])
     total = len(neg) + len(pos)
-    #print("negative:", len(neg), round(len(neg)/total, 2))
-    #print("positive:", len(pos), round(len(pos)/total, 2))
     return Marked(positive=pos, negative=neg)
 
-def get_a_structures(test_part):
+
+def get_a_structures(test_part=0.2):
     train = DataSet()
     test = DataSet()
     marks = get_marked()
@@ -60,8 +94,11 @@ def get_a_structures(test_part):
             post_id, mark = arr[i]
             with open(NORMALIZED_DIR + str(post_id) + '.txt') as file:
                 cur_data = file.read()
+            with open(NORMALIZED_DIR + str(post_id) + '_title.txt') as file:
+                cur_title = file.read()
             if (i <= for_test):
                 test.data.append(cur_data)
+                test.titles.append(cur_title)
                 test.target.append(mark)
                 test.target_names.append(name)
                 if not (name == 'Negative' and first_elements):
@@ -72,11 +109,10 @@ def get_a_structures(test_part):
                 test.target_names[0], test.target_names[-1] = test.target_names[-1], test.target_names[0]
             else:
                 train.data.append(cur_data)
+                train.titles.append(cur_title)
                 train.target.append(mark)
                 train.target_names.append(name)
         name = 'Negative'
-    #print("train:", len(train.data))
-    #print("test:", len(test.data))
     return Articles(train=train, test=test)
 
 
@@ -85,43 +121,108 @@ def scorer(estimator, X, Y):
     return metric(Y, estimator.predict_proba(X)[:, 1])
 
 
-def train_classifier():
-    print('Making sets for train and test')
-    full_struct = get_a_structures()
-    train, test = full_struct.train, full_struct.test
+def choose_the_classifier(classifiers_to_check, data, target):
+    best_classifier = None
+    best_score = 0;
+    best_name = ''
+    for cur_classifier in classifiers_to_check:
+        log.debug('Now training ' + cur_classifier.name)
+        searcher = GridSearchCV(estimator=cur_classifier.clf,
+                                param_grid=cur_classifier.parameters,
+                                scoring=scorer,
+                                cv=3,
+                                n_jobs=-1
+                                )
+        t = time()
+        searcher.fit(data, target)
+        t = time() - t
+        log.debug("{} min, {} sec".format(int(t // 60), round(t % 60, 3)))
+        cur_best_score = searcher.best_score_
+        log.debug("best score (auc): {}".format(round(cur_best_score, 3)))
+        log.debug("best parameters: {}".format(searcher.best_params_))
+        if cur_best_score > best_score:
+            best_score = cur_best_score
+            best_classifier = searcher.best_estimator_
+            best_name = cur_classifier.name
+    return BestClassifier(clf=best_classifier, name=best_name)
 
-    text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-                         ('tfidf', TfidfTransformer()),
-                         ('clf', SGDClassifier(loss='log'))
-                        ])
-    parameters = {'clf__penalty': ['l2', 'l1', 'elasticnet'],
-                  'clf__l1_ratio': [0.0, 0.01, 0.05], # 0.10, 0.2, 0.3, 0.4, 0.5],
-                  'clf__alpha': [0.001], #0.0001, 0.00001, 0.000001, 0.0000001],
-                  'tfidf__use_idf': (True, False)
-                 }
 
-    searcher = GridSearchCV(estimator=text_clf,
-                            param_grid=parameters,
-                            scoring=scorer,
-                            cv=5,
-                            n_jobs=-1
-                           )
-    print('Training... ', end='')
-    t = time()
-    searcher.fit(train.data, train.target)
-    print(round(time() - t, 3), "sec")
+def train_classifier(step_num=1):
+    average = 0
+    for step in range(step_num):
+        print("==STEP {}==".format(step + 1))
 
-    print("\nbest score (auc):", round(searcher.best_score_, 3))
-    print("best parameters:", searcher.best_params_, sep='\n')
-    best_cls = searcher.best_estimator_
-    print("\npredicted quality".upper())
-    predicted = best_cls.predict(test.data)
-    print("auc:", round(scorer(best_cls, test.data, test.target), 3))
-    print("mean:", round(np.mean(predicted == test.target), 3))
-    print(metrics.confusion_matrix(test.target, predicted))
-    print(metrics.classification_report(test.target, predicted,
-                                         target_names=test.target_names))
+        sgd, multi_nb, bernoulli_nb = ClassifierData(), ClassifierData(), ClassifierData()
+        sgd.name = 'SGDClassifier'
+        multi_nb.name = 'MultinomialNB'
+        bernoulli_nb.name = 'BernoulliNB'
+        sgd.clf = Pipeline([('cnt', TfidfVectorizer()),
+                            ('clf', SGDClassifier(loss='log'))
+                           ])
+        multi_nb.clf = Pipeline([('cnt', TfidfVectorizer()),
+                                 ('clf', MultinomialNB())
+                                ])
+        bernoulli_nb.clf = Pipeline([('cnt', CountVectorizer()),
+                                     ('clf', BernoulliNB(binarize=0.0))
+                                    ])
+        sgd.parameters = {'clf__penalty': ['l2', 'l1', 'elasticnet'],
+                          'clf__l1_ratio': [0.0, 0.01, 0.05, 0.10, 0.2, 0.3, 0.4, 0.5],
+                          'clf__alpha': [0.001, 0.0001, 0.00001, 0.000001, 0.0000001],
+                          'cnt__use_idf': (True, False),
+                         }
+        multi_nb.parameters = {'clf__alpha': [0.001, 0.0001, 0.00001, 0.000001, 0.0000001],
+                               'cnt__use_idf': (True, False),
+                              }
+        bernoulli_nb.parameters = {'clf__alpha': [0.001, 0.0001, 0.00001, 0.000001, 0.0000001],
+                                  }
+        all_classifiers = [sgd, multi_nb, bernoulli_nb]
+
+        full_struct = get_a_structures()
+        train, test = full_struct.train, full_struct.test
+
+        print('Training... ', end='')
+        t = time()
+        log.debug("Train with titles:")
+        best_titles_clf = choose_the_classifier(all_classifiers, train.titles, train.target)
+        log.debug("Train with text:")
+        for clf in all_classifiers:
+            clf.parameters['cnt__max_df'] = [0.85, 0.9, 0.95, 1.0]
+            clf.parameters['cnt__min_df'] = [0.01, 0.05, 0.10, 0.15, 1]
+        best_texts_clf = choose_the_classifier(all_classifiers, train.data, train.target)
+        t = time() - t
+        print("{} min, {} sec".format(int(t // 60), round(t % 60, 3)))
+
+
+        print("\npredicted quality".upper())
+        print("best for titles:", best_titles_clf.name)
+        print("best for text:", best_texts_clf.name)
+        pred_titles = best_titles_clf.clf.predict_proba(test.titles)
+        pred_texts = best_texts_clf.clf.predict_proba(test.data)
+        predicted_proba = list()
+        predicted_target = list()
+        for i in range(len(test.data)):
+            prob_neg = 0.5 * (pred_titles[i][0] + pred_texts[i][0])
+            prob_pos = 0.5 * (pred_titles[i][1] + pred_texts[i][1])
+            predicted_proba.append(prob_pos)
+            if prob_neg > prob_pos:
+                predicted_target.append(-1)
+            else:
+                predicted_target.append(1)
+
+
+        cur_auc = metrics.roc_auc_score(test.target, predicted_proba)
+        average += cur_auc
+        print("auc:", round(cur_auc, 3))
+        print(metrics.confusion_matrix(test.target, predicted_target))
+        print(metrics.classification_report(test.target, predicted_target,
+                                             target_names=test.target_names))
+
+    print('==AVERAGE SCORE==')
+    print(round(average / step_num, 3))
 
 
 if __name__ == "__main__":
-    train_classifier()
+    args = parse_argument()
+    log.config(log.level(args.log_level))
+
+    train_classifier(step_num=args.steps)
--- source/normalizing.py
+++ source/normalizing.py
@@ -7,36 +7,53 @@ import re
 import log
 from collections import defaultdict
 import math
+import argparse
+import os
 
+import log
 from config import *
 from utils import *
 
-REG = r'[\u0400-\u0500a-z\d-]{2,}'
-RUS_REG = r'[\u0400-\u0500a-z\d-]{4,}'
-ENG_REG = r'[a-z\d-]{2,}'
-BAD_REG = r'([\d-]+|.*-)'
+
+REG = r'\w+[-\w+]*'
+EMPIRICAL_REG = r'[\u0400-\u0500a-z\d-]{2,}'
+EMP_RUS_REG = r'[\u0400-\u0500a-z\d-]{4,}'
+EMP_ENG_REG = r'[a-z\d-]{2,}'
+EMP_BAD_REG = r'([\d-]+|.*-)'
+
+BANNES_POS = ['NPRO', 'PRED', 'PREP', 'CONJ', 'PRCL', 'INTJ']  # auxiliary parts of speech
+
+def parse_argument():
+    normalizing = argparse.ArgumentParser(
+        prog='normalizing',
+        description="Making the bag of words with normal forms.",
+    )
+    normalizing.add_argument(
+        '--log',
+        default='error',
+        choices=['critical', 'error',  'debug'],
+        dest='log_level',
+        help="log level, 'error' is default value"
+    )
+    normalizing.add_argument(
+        '--empirical',
+        action='store_true',
+        dest='empirical',
+        help="what kind of regular expression is used"
+    )
+    normalizing.add_argument(
+        '--clean',
+        action='store_true',
+        dest='clean',
+        help="delete all normalized articles"
+    )
+
+    return normalizing.parse_args()
 
 
 def store_index(normalized):
     with open(NORMALIZED_INDEX, 'w') as file:
         file.write('\n'.join(map(str, normalized)))
-
-
-def make_normalized_file(post_id, morph):
-    normal_words = list()
-    input_file = TEXT_DIR + str(post_id) + '.txt'
-    output_file = NORMALIZED_DIR + str(post_id) + '.txt'
-    with open(input_file, encoding='utf-8') as fin:
-        lines = fin.readlines()
-        for line in lines[5:]:  # on this line article begins
-            for word in re.findall(REG, line.lower()):
-                normal = morph.parse(word)[0].normal_form
-                if (not re.match(BAD_REG, normal) and
-                       (re.match(RUS_REG, normal) or re.match(ENG_REG, normal))):
-                    normal_words.append(normal)
-    with open(output_file, 'w') as fout:
-        fout.write('\n'.join(normal_words))
-    print("Post {} normalized".format(post_id))
 
 
 def get_marked():
@@ -47,15 +64,74 @@ def get_marked():
             result.add(post_id)
     return result
 
-def normalizing():
+
+def delete_normalized():
+    with open(NORMALIZED_INDEX, 'w') as file:
+        file.write('')
+    for file in os.listdir(NORMALIZED_DIR):
+        if file.endswith('.txt'):
+            os.remove(NORMALIZED_DIR + file)
+
+def get_words_from_line(line, morph, empirical):
+    store_list = list()
+    line = line.lower().replace(chr(1105), chr(1077))  # ё -> е
+    if not empirical:
+        all_words = re.findall(REG, line)
+    else:
+        all_words = re.findall(EMPIRICAL_REG, line)
+    for word in all_words:
+        parsed_word = morph.parse(word)[0]
+        if not empirical:
+            if (parsed_word.tag.POS not in BANNES_POS):
+                store_list.append(parsed_word.normal_form)
+        else:
+            normal = parsed_word.normal_form
+            if (not re.match(EMP_BAD_REG, normal) and
+                    (re.match(EMP_RUS_REG, normal) or re.match(EMP_ENG_REG, normal))):
+                store_list.append(normal)
+    return store_list
+
+
+def make_normalized_file(post_id, morph, empirical):
+    input_file = TEXT_DIR + str(post_id) + '.txt'
+    output_file = NORMALIZED_DIR + str(post_id) + '.txt'
+    output_title_file = NORMALIZED_DIR + str(post_id) + '_title.txt'
+    with open(input_file, encoding='utf-8') as fin:
+        lines = fin.readlines()
+
+        # title
+        words = get_words_from_line(lines[4], morph, empirical=True)
+        with open(output_title_file, 'w') as fout:
+            fout.write('\n'.join(words))
+
+        # keywords
+        words = get_words_from_line(lines[2], morph, empirical)
+        with open(output_file, 'a') as fout:
+            for step in range(5):  # keywords have higher weight
+                fout.write('\n'.join(words))
+
+        # articles text
+        words = get_words_from_line(' '.join(lines[6:]), morph, empirical)
+        with open(output_file, 'a') as fout:
+            fout.write('\n'.join(words))
+    log.debug("Post {} normalized".format(post_id))
+
+
+def normalize(empirical=False):
     morph = pymorphy2.MorphAnalyzer()
     cleaned = load_cleaned()
     marked = get_marked()
     normalized = get_normalized()
     for post_id in (cleaned & marked - normalized):
-        make_normalized_file(post_id, morph)
+        make_normalized_file(post_id, morph, empirical)
         normalized.add(post_id)
     store_index(normalized)
 
+
 if __name__ == '__main__':
-    normalizing()
+    args = parse_argument()
+    log.config(log.level(args.log_level))
+    if args.clean:
+        delete_normalized()
+    else:
+        normalize(args.empirical)
